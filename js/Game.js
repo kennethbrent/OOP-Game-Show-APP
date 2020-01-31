34d121e6214882a5223f713491ef376a2a9c2272
kennethbrent <kennethbrentthomas@gmail.com>
2020-01-30 23:07:33 -0500

    Added Phrase and Game classes.

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,22 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+class Game {
+    constructor(){
+        this.missed = 0,
+        this.phrases = [{phrase: 'Iron Man'}, {phrase: 'Black Widow'}, {phrase:'Spiderman'}, {phrase: 'Thanos'}, {phrase: 'Doctor Stange'}]
+        this.activePhrase = null
+    }
+
+    getRandomPhrase(){
+        const phrase = this.phrases[Math.floor(Math.random() * this.phrases.length)];
+        return new Phrase(phrase.phrase);
+    }
+
+    startGame(){
+        $('#overlay').hide();
+        const phrase = this.getRandomPhrase()
+        this.activePhrase = phrase.phrase
+        phrase.addPhraseToDisplay()
+    }
+}
--- js/Phrase.js
+++ js/Phrase.js
@@ -1,3 +1,25 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Phrase.js */
+class Phrase {
+    constructor(phrase){
+        this.phrase = phrase.toLowerCase()
+    }
+
+    addPhraseToDisplay(){
+       const phraseArray = this.phrase.split("");
+       const phrase_container = $('#phrase ul')
+       phraseArray.forEach((letter) => {
+           if(letter != ' '){
+            phrase_container.append(`
+            <li class="hide letter ${letter}">${letter}</li>
+            `)
+           } else {
+            phrase_container.append(`
+             <li class="space"></li>
+            `)
+        }
+
+       })
+    }
+}
